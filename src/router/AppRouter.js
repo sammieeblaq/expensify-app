4fcc62b0c81f611edab0723bd6d7c50abc9ae850
sammieeblaq <odianoseairehrour@gmail.com>
2019-08-02 07:39:55 +0100

    added routes

---
+++ src/router/AppRouter.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { BrowserRouter, Route, Switch} from "react-router-dom";
+
+// components
+import Header from "../components/Header";
+
+// pages
+import ExpenseDashBoadPage from "../components/pages/ExpenseDashboardPage";
+import AddExpensePage from "../components/pages/AddExpensePage";
+import EditExpensePage from "../components/pages/EditExpensePage";
+import HelpPage from "../components/pages/HelpPage";
+import NotFoundPage from "../components/pages/NotFoundPage";
+
+
+
+const AppRouter = () => (
+    <BrowserRouter>
+        <div>
+            <Header />
+            <Switch>
+                <Route path="/" component={ ExpenseDashBoadPage } exact />
+                <Route path="/create" component={ AddExpensePage } />
+                <Route path="/edit" component={ EditExpensePage } />
+                <Route path="/help" component={ HelpPage } />
+                <Route component={ NotFoundPage } />
+            </Switch>
+        </div>
+    </BrowserRouter>
+)
+
+export default AppRouter;
