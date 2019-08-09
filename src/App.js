4044e66ea48807aca979d4fd0633473a0e39e9fa
sammieeblaq <odianoseairehrour@gmail.com>
2019-08-09 19:03:29 +0100

    Added more pages

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Provider } from "react-redux";
 
 import configureStore from "./store/configStore";
 import { addExpense } from "./actions/expenses";
-import { setTextFilter } from "./actions/filters";
+// import { setTextFilter } from "./actions/filters";
 import getVisibleExpenses from "./selectors/expenses";
 import AppRouter from "./router/AppRouter";
 
@@ -16,12 +16,8 @@ class App extends Component {
         const store = configureStore();
 
         store.dispatch(addExpense({ description: "Water bill", amount: 3400 }))
-        store.dispatch(addExpense({ description: "Gas bill" }))
-        store.dispatch(setTextFilter("water"));
-
-        setTimeout(() => {
-            store.dispatch(setTextFilter("bill"));
-        }, 3000);
+        store.dispatch(addExpense({ description: "Gas bill", createdAt: 1000 }))
+        store.dispatch(addExpense({ description: "Rent", amount: 109500 }))
 
         const state = store.getState();
         const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
